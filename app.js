7e1791ce8ef51cb73b4c9f56d7476bb198a4ba33
stroieva <78794764+stroieva@users.noreply.github.com>
2022-01-28 22:09:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let city = prompt("Введіть назву міста");
 console.log(city);
 
 fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=5d066958a60d315387d9492393935c19`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=5d066958a60d315387d9492393935c19`
    )
    .then(function (resp) {
       return resp.json();
@@ -18,7 +18,7 @@ fetch(
       document.querySelector(".wind-speed").textContent =
          "Speed of wind: " + data.wind.speed + " km/h";
       document.querySelector(".degrees").innerHTML = "Degrees: " + data.wind.deg + "&deg;";
-      document.querySelector(".icon").innerHTML = '<img src = "http://openweathermap.org/img/w/' + data.weather[0].icon + '.png">';
+      document.querySelector(".icon").innerHTML = '<img src = "https://openweathermap.org/img/w/' + data.weather[0].icon + '.png">';
       document.querySelector(".temperature").innerHTML = "Temperature: " +
          Math.round(data.main.temp) + "&deg;" + " C";
       document.querySelector(".description").textContent = data.weather[0].description;
